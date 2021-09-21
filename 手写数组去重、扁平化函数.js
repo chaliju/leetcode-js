271d4501f7d89c74e0f4cb74760e5a68a328952b
chaliju <chaliju1997@163.com>
2021-09-21 21:50:13 +0800

    手写数组扁平化、去重

--- 手写数组去重、扁平化函数.js
+++ 手写数组去重、扁平化函数.js
@@ -25,5 +25,90 @@ test.flat(-10)
 // 如果原数组有空位，flat()方法会跳过空位。
 ["a", "b", "c", "d",,].flat()
 // ["a", "b", "c", "d"]
-
  */
+
+/** 方法一：使用reduce方法 */
+// 一次性扁平化所有
+// reduce() 方法对数组中的每个元素执行一个由您提供的reducer函数(升序执行)，将其结果汇总为单个返回值。
+// accumulator:累计器累计回调的返回值; 它是上一次调用回调时返回的累积值，或initialValue（见于下方）
+// currentValue:数组中正在处理的元素
+function flattenDeep(arr) {
+  return Array.isArray(arr) ? arr.reduce((acc, cur) => [...acc, ...flattenDeep(cur)], []) : [arr]
+}
+
+// 测试
+var test = ["a", ["b", "c"],
+  ["d", ["e", ["f"]], "g"]
+]
+flattenDeep(test)
+// ["a", "b", "c", "d", "e", "f", "g"]
+
+
+// 实现flat函数：
+function flat(arr, depth = 1) {
+  return depth > 0 ? arr.reduce((acc, cur) => {
+    if (Array.isArray(cur)) {
+      return [...acc, ...flat(cur, depth - 1)]
+    }
+    return [...acc, cur]
+  }, []) : arr
+}
+
+/** 方法二：栈——一次性降维所有 */
+function flattenDeep(arr) {
+  const result = []
+  //将数组元素拷贝至栈，直接赋值会改变原数组
+  const stack = [...arr]
+  // 如果栈不为空，则循环遍历
+  while (stack.length !== 0) {
+    const val = stack.pop()
+    if (Array.isArray(val)) {
+      // 如果是数组再次入栈，并且展开了一层
+      stack.push(...val)
+    } else {
+      // 如果不是数组，就用头插法插入到结果数组中
+      result.unshift(val)
+    }
+  }
+  return result
+}
+
+
+// ------------------------------------------------------
+// 数组去重
+/** 方法一：Set(ES6) */
+function unique(arr) {
+  return Array.from(new Set(arr))
+}
+
+// 或者
+var unique = arr => [...new Set(arr)]
+
+// 测试
+var arr = [1, 2, 2, 3]
+unique(arr); // [1, 2, 3]
+
+/** 方法二：reduce */
+function unique(arr) {
+  return arr.sort().reduce((acc, cur) => {
+    if (acc.length === 0 || acc[acc.length - 1] !== cur) {
+      acc.push(cur)
+    }
+    return acc
+  }, [])
+}
+
+// 测试
+var arr = [1, 2, 2, 3]
+unique(arr); // [1,2,3]
+
+/** 方法三：filter */
+function unique(arr) {
+  return arr.filter((element, index, array) => {
+    return array.indexOf(element) === index
+  })
+}
+
+// 测试
+var arr = [1, 2, 2, 3]
+unique(arr) //[1,2,3]
